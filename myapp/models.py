7624f46c1c2cd2d46705e2f0f255739104ffd116
Bishal Gautam <bisalgt@gmail.com>
2022-09-24 21:17:31 +0200

    Constraints added for age field on MyModel inside of myapp. Generates Migration file containing '.'(dot) in the name. This migration file is not detected by django. So everytime you do makemigraitons, it generates the same file again and again.

--- myapp/models.py
+++ myapp/models.py
@@ -5,10 +5,10 @@ class MyModel(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
-    # class Meta:
-    #     constraints = [
-    #         models.CheckConstraint(
-    #             check=models.Q(age__gte=1),
-    #             name="Age should not be. less.than one."
-    #         )
-    #     ]
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(age__gte=1),
+                name="Age should not be. less.than one."
+            )
+        ]
