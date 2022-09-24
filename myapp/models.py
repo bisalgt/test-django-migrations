96990f4389f6ea5a743261f6ba4ef0d664de438c
Bishal Gautam <bisalgt@gmail.com>
2022-09-24 21:14:13 +0200

    Added: App added to project with example model.

---
+++ myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    # class Meta:
+    #     constraints = [
+    #         models.CheckConstraint(
+    #             check=models.Q(age__gte=1),
+    #             name="Age should not be. less.than one."
+    #         )
+    #     ]
